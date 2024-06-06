da3f24f55df66e6bd6b5642f0c65a87cdc92c032
Htet Aung Kyaw <htet.aung.kyaw@use-inc.co.jp>
2024-06-06 19:50:09 +0900

    connect and discover

--- app/src/main/java/it/kyaw/bleapp/DeviceAdapter.java
+++ app/src/main/java/it/kyaw/bleapp/DeviceAdapter.java
@@ -12,7 +12,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -21,42 +20,157 @@ import java.util.List;
 
 public class DeviceAdapter extends RecyclerView.Adapter<DeviceAdapter.ViewHolder> {
 
-    private List<String> deviceList;
-
+    //    private List<Device> deviceList;
+//
     private Context context;
+//
+//    private OnItemClickListener listener;
+//
+//    public interface OnItemClickListener {
+//        void onItemClick(Device device);
+//    }
+//
+//    public DeviceAdapter(List<Device>deviceList,OnItemClickListener listener ) {
+////        this.context = context;
+//        this.deviceList = deviceList;
+//        this.listener = listener;
+//
+//    }
+//
+//    @NonNull
+//    @Override
+//    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_devices, parent, false);
+//        return new ViewHolder(view);
+//
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+////        String deviceName = deviceList.get(position);
+//        Device device = deviceList.get(position);
+//        holder.deviceName.setText(device.getName() != null ? device.getName() : "Unknown Device");
+//        holder.deviceAddress.setText(device.getAddress());
+//        holder.itemView.setOnClickListener(v -> listener.onItemClick(device));
+////        holder.deviceName.setText(device.getName());
+//        holder.menu_btn.setOnClickListener(v -> {
+//            Log.i("Kyaw","name edit dialog..*********");
+//
+//            showAlerDialgo();
+//
+//
+//        });
+//
+//
+//
+//    }
+//
+//    private void showAlerDialgo() {
+//        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+//        LayoutInflater inflater = LayoutInflater.from(context);
+//        View dialogView = inflater.inflate(R.layout.name_edit_box, null);
+//        builder.setView(dialogView);
+//
+//        TextView dialogTitle = dialogView.findViewById(R.id.nameText);
+//        EditText dialogMessage = dialogView.findViewById(R.id.nameChange);
+//        Button buttonOk = dialogView.findViewById(R.id.btn_Ok);
+//        Button buttonCancel = dialogView.findViewById(R.id.btn_Cancel);
+//
+//        dialogTitle.setText("Name Edit...");
+//        dialogMessage.setText("Details about device: " + deviceList);
+//
+//        AlertDialog alertDialog = builder.create();
+//
+//        buttonOk.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                alertDialog.dismiss();
+//                Log.i("Kyaw","OK Button..*********");
+//            }
+//        });
+//
+//        buttonCancel.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v)
+//            {
+//                alertDialog.dismiss();
+//                Log.i("Kyaw","Cancel Button..*********");
+//            }
+//        });
+//
+//        alertDialog.show();
+//    }
 
-    public DeviceAdapter( Context context,List<String> deviceList) {
-        this.context = context;
+//    @Override
+//    public int getItemCount() {
+//        return deviceList.size();
+//    }
+//
+//    public void addDevice(Device device) {
+//        if (!deviceList.contains(device)) {
+//            deviceList.add(device);
+//            notifyDataSetChanged();
+//        }
+//    }
+//
+//    public static class ViewHolder extends RecyclerView.ViewHolder {
+//        TextView deviceName,deviceAddress;
+//        ImageView menu_btn;
+//
+//        public ViewHolder(@NonNull View itemView) {
+//            super(itemView);
+//            deviceName = itemView.findViewById(R.id.devicename);
+//            deviceAddress = itemView.findViewById(R.id.device_address);
+//            menu_btn = itemView.findViewById(R.id.menu_btn);
+//
+//
+//        }
+//    }
+//}
+
+    private List<Device> deviceList;
+    private OnItemClickListener listener;
+
+
+    public DeviceAdapter(List<Device> deviceList, OnItemClickListener listener) {
         this.deviceList = deviceList;
+        this.listener = listener;
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_devices, parent, false);
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_devices, parent, false);
         return new ViewHolder(view);
-
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        String deviceName = deviceList.get(position);
-        holder.deviceName.setText(deviceName);
+    public void onBindViewHolder(@NonNull DeviceAdapter.ViewHolder holder, int position) {
+        Device device = deviceList.get(position);
+        holder.deviceName.setText(device.getName() != null ? device.getName() : "Unknown Device");
+        holder.deviceAddress.setText(device.getAddress());
         holder.menu_btn.setOnClickListener(v -> {
-            Log.i("Kyaw","name edit dialog..*********");
-            
+            Log.i("Kyaw", "name edit dialog..*********");
+
             showAlerDialgo();
-           
+
 
         });
+        holder.itemView.setOnClickListener(v -> {
+            listener.onItemClick(device);
 
+            Log.i("KYAW", " Connected device " + device);
+        });
 
-
+//
+//
     }
 
     private void showAlerDialgo() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
-        LayoutInflater inflater = LayoutInflater.from(context);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this.context);
+        LayoutInflater inflater = LayoutInflater.from(this.context);
         View dialogView = inflater.inflate(R.layout.name_edit_box, null);
         builder.setView(dialogView);
 
@@ -75,41 +189,19 @@ public class DeviceAdapter extends RecyclerView.Adapter<DeviceAdapter.ViewHolder
             public void onClick(View v) {
 
                 alertDialog.dismiss();
-                Log.i("Kyaw","OK Button..*********");
+                Log.i("Kyaw", "OK Button..*********");
             }
         });
 
         buttonCancel.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 alertDialog.dismiss();
-                Log.i("Kyaw","Cancel Button..*********");
+                Log.i("Kyaw", "Cancel Button..*********");
             }
         });
 
         alertDialog.show();
-
-
-//        builder.setTitle("Device Name Change");
-//        builder.setMessage("Are you sure device name change??");
-//
-//        builder.setPositiveButton("OK", (dialog, which) -> {
-//            Toast.makeText(context, "OK clicked", Toast.LENGTH_SHORT).show();
-//        });
-//
-//        builder.setNegativeButton("Cancel", (dialog, which) -> {
-//            Toast.makeText(context, "Cancel clicked", Toast.LENGTH_SHORT).show();
-//        });
-//
-//        AlertDialog dialog = builder.create();
-//        dialog.show();
-//        AlertDialog.Builder builder = new AlertDialog.Builder(context);
-//        builder.setTitle("Device Information")
-//                .setMessage("Details about the device: " )
-//                .setPositiveButton("OK", null)
-//                .setNegativeButton("Cancel", null);
-//        builder.show();
     }
 
     @Override
@@ -117,16 +209,30 @@ public class DeviceAdapter extends RecyclerView.Adapter<DeviceAdapter.ViewHolder
         return deviceList.size();
     }
 
+    public void addDevice(Device device) {
+        if (!deviceList.contains(device)) {
+            deviceList.add(device);
+            notifyDataSetChanged();
+        }
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(Device device);
+    }
+
     public static class ViewHolder extends RecyclerView.ViewHolder {
         TextView deviceName;
+        TextView deviceAddress;
+
         ImageView menu_btn;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             deviceName = itemView.findViewById(R.id.devicename);
+            deviceAddress = itemView.findViewById(R.id.device_address);
             menu_btn = itemView.findViewById(R.id.menu_btn);
-
-
         }
     }
+
+
 }
--- app/src/main/java/it/kyaw/bleapp/MainActivity.java
+++ app/src/main/java/it/kyaw/bleapp/MainActivity.java
@@ -1,23 +1,27 @@
 package it.kyaw.bleapp;
 
-//package com.example.bleapp;
-
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanResult;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,153 +32,221 @@ import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_ENABLE_BT = 1;
     private static final int REQUEST_FINE_LOCATION = 2;
+    private static final long SCAN_PERIOD = 5000;
+    private final BluetoothGattCallback gattCallback = new BluetoothGattCallback() {
+        @SuppressLint("MissingPermission")
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            if (newState == BluetoothProfile.STATE_CONNECTED) {
+                Log.i("MainActivity", "Connected to GATT server.");
+                gatt.discoverServices();
+            } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                Log.i("MainActivity", "Disconnected from GATT server.");
+            }
+        }
 
+        @Override
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                Log.i("MainActivity", "Services discovered.");
+            } else {
+                Log.w("MainActivity", "onServicesDiscovered received: " + status);
+            }
+        }
+
+        @Override
+        public void onCharacteristicRead(BluetoothGatt gatt,
+                                         BluetoothGattCharacteristic characteristic,
+                                         int status) {
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                Log.i("MainActivity", "Characteristic read.");
+            }
+        }
+    };
+    Button stoptbtn, startbtn;
+    ImageView bt_search;
+    TextView bt_text, text_date_time;
+    ProgressBar progress_bar;
     private BluetoothAdapter bluetoothAdapter;
     private BluetoothLeScanner bluetoothLeScanner;
-    private DeviceAdapter deviceAdapter;
-    private List<String> deviceList = new ArrayList<>();
     private boolean isScanning = false;
-
-    Button stoptbtn,startbtn;
-    ImageView bt_search;
-    TextView bt_text;
-
-//    private static final String DEVICE_UUID = "00001101-0000-1000-8000-00805F9B34FB";
-
-    private static final long SCAN_PERIOD = 3000;
-
     private Handler handler;
+    private DeviceAdapter deviceAdapter;
+    private final ScanCallback leScanCallback = new ScanCallback() {
+        @SuppressLint("MissingPermission")
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            super.onScanResult(callbackType, result);
+            BluetoothDevice bluetoothDevice = result.getDevice();
+            @SuppressLint("MissingPermission") Device device = new Device(
+                    bluetoothDevice.getName() != null ? bluetoothDevice.getName() : "Unknown Device",
+                    bluetoothDevice.getAddress());
+            Log.i("Kyaw", "Discovered divice name :" + bluetoothDevice.getName() + "\n" + "Device address " + bluetoothDevice.getAddress());
+            deviceAdapter.addDevice(device);
+        }
+    };
+    private List<Device> deviceList = new ArrayList<>();
 
-
-
-
-    @SuppressLint("ObsoleteSdkInt")
+    @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Request permissions
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_FINE_LOCATION);
-            }
-        }
-
-        // Initialize Bluetooth
-        BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
-        bluetoothAdapter = bluetoothManager.getAdapter();
-
-        bluetoothLeScanner = bluetoothAdapter.getBluetoothLeScanner();
-
-        // Initialize RecyclerView
-        RecyclerView recyclerView = findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        deviceAdapter = new DeviceAdapter(this,deviceList);
-        recyclerView.setAdapter(deviceAdapter);
         initUI();
         initListener();
+        // Display the current date and time
+        displayCurrentDateTime();
+
+        // Initialize Bluetooth
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        bluetoothAdapter = bluetoothManager.getAdapter();
+
+        if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
+            // Bluetooth is not enabled
+            startActivityForResult(new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE), REQUEST_ENABLE_BT);
+        }
+
+        handler = new Handler();
+        bluetoothLeScanner = bluetoothAdapter.getBluetoothLeScanner();
+    }
+
+    private void displayCurrentDateTime() {
+        // Get the current date and time
+        Date currentDate = new Date();
+
+        // Format the date and time
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss", Locale.getDefault());
+        String formattedDate = dateFormat.format(currentDate);
+
+        // Display the formatted date and time in the TextView
+        text_date_time.setText(formattedDate);
 
     }
 
-    @SuppressLint("SetTextI18n")
     private void initListener() {
-        startbtn.setOnClickListener(view -> {
-                    if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
-                        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                        startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-                    }else{
 
+        startbtn.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                startScan();
                 startbtn.setText("Scanning");
-                bt_text.setText("Bluetooth Device Searching...");
-
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        startScan();
-                    }
-                },SCAN_PERIOD);
-
-                Log.i("kyaw","scanning timeout 3s");
-
-
-        }});
-        stoptbtn.setOnClickListener(view -> {
-            startbtn.setText("Start");
-            bt_text.setText("Bluetooth Disable");
-            stopScan();
-
+                bt_text.setText("BLE Device Searching......");
+            }
         });
+
+        stoptbtn.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                stopScan();
+                startbtn.setText("Start");
+                deviceList.clear();
+
+            }
+        });
+
+
     }
 
     private void initUI() {
 
-        // Initialize buttons
         startbtn = findViewById(R.id.startbtn);
         stoptbtn = findViewById(R.id.stoptbtn);
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
         bt_search = findViewById(R.id.bt_search);
-        bt_text= findViewById(R.id.bt_text);
+        bt_text = findViewById(R.id.bt_text);
+        progress_bar = findViewById(R.id.progress_bar);
+        text_date_time = findViewById(R.id.text_date_time);
 
-        handler = new Handler();
-
+        // Initialize RecyclerView
+        deviceAdapter = new DeviceAdapter(deviceList, this::connectToDevice);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(deviceAdapter);
     }
 
-    @SuppressLint({"MissingPermission", "NotifyDataSetChanged"})
-    private void startScan() {
-        if (!isScanning) {
-            isScanning = true;
-            deviceList.clear();
-            deviceAdapter.notifyDataSetChanged();
-            bluetoothLeScanner.startScan(scanCallback);
-            Toast.makeText(this, "Scanning started...", Toast.LENGTH_SHORT).show();
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_FINE_LOCATION);
         }
+    }
+
+    @SuppressLint("MissingSuperCall")
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_FINE_LOCATION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted
+                startScan();
+            } else {
+                // Permission denied
+                Toast.makeText(this, "Location permission is required for BLE scanning", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @SuppressLint({"SetTextI18n", "MissingPermission", "NotifyDataSetChanged"})
+    private void startScan() {
+        Log.i("Kyaw", "StartScanning.......");
+        if (isScanning) return;
+
+        // Stops scanning after  5 seconds scan period.
+        handler.postDelayed(() -> {
+            isScanning = false;
+            bluetoothLeScanner.stopScan(leScanCallback);
+            startbtn.setText("Start");
+            bt_text.setText("BLE Disable......");
+            progress_bar.setVisibility(View.GONE);
+            bt_search.setImageResource(R.drawable.ic_bt_disabled);
+            Toast.makeText(MainActivity.this, "Scan stopped", Toast.LENGTH_SHORT).show();
+        }, SCAN_PERIOD);
+
+
+        //Scanning 5 seconds
+        isScanning = true;
+        deviceList.clear();
+        deviceAdapter.notifyDataSetChanged();
+        bluetoothLeScanner.startScan(leScanCallback);
+        progress_bar.setVisibility(View.VISIBLE);
+        Toast.makeText(this, "Scanning 5seconds...", Toast.LENGTH_SHORT).show();
         bt_search.setImageResource(R.drawable.ic_bt_searching);
-        Log.i("Kyaw", "Start Scanning..........");
     }
 
     @SuppressLint("MissingPermission")
     private void stopScan() {
+        Log.i("Kyaw", "Stoped Scanning.......");
         if (isScanning) {
             isScanning = false;
             deviceList.clear();
-            bluetoothLeScanner.stopScan(scanCallback);
-            Toast.makeText(this, "Scanning stopped.", Toast.LENGTH_SHORT).show();
+            bluetoothLeScanner.stopScan(leScanCallback);
+            Toast.makeText(this, "Scan stopped", Toast.LENGTH_SHORT).show();
         }
+
         bt_search.setImageResource(R.drawable.ic_bt_disabled);
-        Log.i("Kyaw", "Stop Scanning...........");
+//        Log.i("Kyaw", "Stop Scanning...........");
     }
 
-    private final ScanCallback scanCallback = new ScanCallback() {
-        @SuppressLint("NotifyDataSetChanged")
-        @Override
-        public void onScanResult(int callbackType, ScanResult result) {
-            super.onScanResult(callbackType, result);
-            BluetoothDevice device = result.getDevice();
-            @SuppressLint("MissingPermission") String deviceName = device.getName();
-            if (deviceName != null && !deviceList.contains(deviceName)) {
-                deviceList.add(deviceName);
-                deviceAdapter.notifyDataSetChanged();
-                Log.i("Kyaw","Discovered divice name :" + deviceName);
-
-            }
-        }
-    };
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == REQUEST_FINE_LOCATION) {
-            if (grantResults.length > 0 && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(this, "Location permission is required for BLE scanning.", Toast.LENGTH_SHORT).show();
-//                finish();
-            }
-        }
+    private void connectToDevice(Device device) {
+        BluetoothDevice bluetoothDevice = bluetoothAdapter.getRemoteDevice(device.getAddress());
+        @SuppressLint("MissingPermission") BluetoothGatt bluetoothGatt = bluetoothDevice.connectGatt(this, false, gattCallback);
     }
 }
